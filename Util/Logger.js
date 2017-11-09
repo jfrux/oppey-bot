a7afa79831127ea970998e1f25c9963e5a5fc5a2
Soumil07 <soumil.paranjpay@gmail.com>
2017-11-09 16:47:44 +0530

    codacy

--- Util/Logger.js
+++ Util/Logger.js
@@ -1,40 +1,39 @@
 /*
 Logger class for easy and aesthetically pleasing console logging
 */
-const chalk = require('chalk');
-const moment = require('moment');
+const chalk = require("chalk");
+const moment = require("moment");
 
 class Logger {
-  static log(content, type = 'log') {
-    const timestamp = `[${moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
-    const bold = chalk.bold(`[${tag}]`);
-    switch(type) {
-      case 'log': {
-        return console.log(`${timestamp} ${chalk.bgBlue(tag)} ${content} `);
+  static log(content, type = "log") {
+    const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
+    switch (type) {
+      case "log": {
+        return console.log(`${timestamp} ${chalk.bgBlue(type)} ${content} `);
       }
-      case 'warn': {
-        return console.log(`${timestamp} ${chalk.black.bgYellow(tag)} ${content} `);
+      case "warn": {
+        return console.log(`${timestamp} ${chalk.black.bgYellow(type)} ${content} `);
       }
-      case 'error': {
-        return console.log(`${timestamp} ${chalk.bgRed(tag)} ${content} `);
+      case "error": {
+        return console.log(`${timestamp} ${chalk.bgRed(type)} ${content} `);
       }
-      case 'debug': {
-        return console.log(`${timestamp} ${chalk.green(tag)} ${content} `);
+      case "debug": {
+        return console.log(`${timestamp} ${chalk.green(type)} ${content} `);
       } 
-      default: throw new TypeError('Logger type must be either warn, debug, log or error.');
+      default: throw new TypeError("Logger type must be either warn, debug, log or error.");
     } 
   }
   
   static error(content) {
-    return this.log(content, 'error');
+    return this.log(content, "error");
   }
   
   static warn(content) {
-    return this.log(content, 'warn');
+    return this.log(content, "warn");
   }
   
   static debug(content) {
-    return this.log(content, 'debug');
+    return this.log(content, "debug");
   } 
 }
 
