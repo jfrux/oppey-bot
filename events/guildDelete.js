9eb367e35015d74cc731080ca78b1c69e452464b
Soumil07 <soumil.paranjpay@gmail.com>
2018-02-01 21:23:19 +0530

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -8,7 +8,8 @@ module.exports = class {
   async run(guild) {
 
     this.client.user.setActivity(`${this.client.settings.get("default").prefix}help | ${this.client.guilds.size} Servers`);
-    // Well they're gone. Let's remove them from the settings!
+    // Well they're gone. Let's remove them from the settings and log it!
     this.client.settings.delete(guild.id);
+    this.client.logger.log(`Left guild: ${guild.name} (${guild.id}) with ${guild.memberCount} members`);
   }
 };
