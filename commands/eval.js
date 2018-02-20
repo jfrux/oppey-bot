14247779a589d5acdab99e9cca67673b023eee16
Soumil07 <soumil.paranjpay@gmail.com>
2018-02-20 21:33:29 +0530

    eval enhancement

--- commands/eval.js
+++ commands/eval.js
@@ -25,6 +25,12 @@ class Eval extends Command {
     try {
       const evaled = eval(code);
       const clean = await this.client.clean(this.client, evaled);
+      // sends evaled output as a file if it exceeds the maximum character limit
+      // 6 graves, and 2 characters for "js"
+      const MAX_CHARS = 3 + 2 + clean.length + 3;
+      if (MAX_CHARS > 2000) {
+        message.channel.send("Output exceeded 2000 characters. Sending as a file.", { files: [{ attachment: Buffer.from(clean), name: "output.txt" }] });
+      }
       message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
