2f8a8d7e7b9aa1fa275af9905c158cdfc24a6d74
York <YorkAARGH@users.noreply.github.com>
2018-09-08 15:20:16 +0100

    SMFH
    I can't believe I didn't notice this sooner!

--- index.js
+++ index.js
@@ -191,6 +191,6 @@ const init = async () => {
 init();
 
 client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
-  .on("reconnect", () => client.logger.log("Bot reconnecting...", "log"))
+  .on("reconnecting", () => client.logger.log("Bot reconnecting...", "log"))
   .on("error", e => client.logger.error(e))
   .on("warn", info => client.logger.warn(info));
