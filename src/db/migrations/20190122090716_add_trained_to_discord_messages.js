bc4f769cedc68724df93d01d98e85f113bf42198
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-08 12:10:16 -0500

    Fixes migration file

--- src/db/migrations/20190122090716_add_trained_to_discord_messages.js
+++ src/db/migrations/20190122090716_add_trained_to_discord_messages.js
@@ -1,7 +1,10 @@
 module.exports = function (){
   try {
     this.addColumn('discord_messages', function(){
-      this.boolean('trained');
+      this.boolean('trained',{
+        not_null: true,
+        default: false
+      });
     })
   } catch (e) {
     console.log("[ERR] Could not add trained to discord_messages.");
